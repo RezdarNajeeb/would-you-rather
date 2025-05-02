7083050cb1126f137f095355e49960f6c7adbfb4
Rezdar Najeeb <125973154+RezdarNajeeb@users.noreply.github.com>
2025-05-02 03:06:39 +0300

    add initial project setup with React, Firebase integration, and basic components

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAi56vVxMQrMGbTkIpddp54AzEEEDpp8TQ",
@@ -9,9 +8,8 @@ const firebaseConfig = {
   storageBucket: "would-you-rather-99e71.firebasestorage.app",
   messagingSenderId: "737670658095",
   appId: "1:737670658095:web:24dbc858725928ec102eda",
-  measurementId: "G-E2KMVTTK3D",
+  measurementId: "G-E2KMVTTK3D"
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
